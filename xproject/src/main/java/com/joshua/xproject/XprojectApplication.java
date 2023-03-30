18ac0dd6fb7ab78c0ceaabdaa2a524be86935e67
joshua <292387082@qq.com>
2023-03-30 14:33:47 +0100

    spring init

---
+++ xproject/src/main/java/com/joshua/xproject/XprojectApplication.java
@@ -0,0 +1,13 @@
+package com.joshua.xproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XprojectApplication.class, args);
+	}
+
+}
---
+++ xproject/src/test/java/com/joshua/xproject/XprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joshua.xproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
